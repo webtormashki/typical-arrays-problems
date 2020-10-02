3562df621d8219aeeaaab859338ca863598c564b
Tanya <webtormashki@gmail.com>
2020-10-02 22:41:03 +0300

    feat: add min calculation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0)  {
+    return 0;
+  }
+  var min = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
